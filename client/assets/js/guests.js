fea07e61ff5b51e6de206e72cdd16562dda2950c
Giovanni Mendoza <mendozagioo@gmail.com>
2019-01-28 19:43:16 -0600

    WIP con la vista de eventos del home

--- client/assets/js/guests.js
+++ client/assets/js/guests.js
@@ -1,7 +1,12 @@
 (function () {
   var app = {
     init: function () {
+      app.interactions();
       app.get_guests();
+    },
+
+    interections: function() {
+      $('#all-guests .btn-info')
     },
 
     get_guests: function() {
@@ -14,18 +19,39 @@
         if (res.length < 1) {
           $('#all-guests > .alert').addClass('alert-info').removeClass('hidden');
         } else {
-          $('#all-guests table').appendChild('<tbody>' + app.add_item_guest(res) +'</tbody>');
+          $('#all-guests table').append('<tbody>' + app.add_item_guest(res) +'</tbody>');
           $('#all-guests table').removeClass('hidden');
         }
       });
     },
 
     add_item_guest: function(items) {
-      console.dir(items);
-      // $('#all-guests table tbody').appendChild(
-      //   '<tr>' +
-      //   '</tr>'
-      // );
+      var guests = '';
+
+      items.forEach(function(val) {
+        guests += '<tr>' +
+          '<th scope="row">'+ val.id_evento +'</th>' +
+          '<td>' + val.usuario +'</td>' +
+          '<td>' + val.nombre +'</td>' +
+          '<td>' + val.primer_apellido +'</td>' +
+          '<td>' + val.segundo_apellido +'</td>' +
+          '<td>' + val.no_invitados +'</td>'+
+          '<td>' +
+            '<button class="btn btn-info" data-toggle="modal" data-target="#guest-modal" data-id="'+ val.id +'">' +
+              '<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>' +
+            '</button>' +
+          '<button class="btn btn-danger" data-toggle="modal" data-target="#confirm-delete-modal" data-id="' + val.id +'">' +
+              '<span class="glyphicon glyphicon-trash" aria-hidden="true"></span>' +
+            '</button>' +
+          '</td>' +
+        '</tr>';
+      });
+
+      return guests;
+    },
+
+    fill_modal: function() {
+      // $('.form-modal .form-control[name="usuario"]').val()
     }
   };
 
