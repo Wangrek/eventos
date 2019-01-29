b78f55a8928eb00383966ef1ee89495bc0d820c8
Wangrek <abraham.saldivar@vun.mx>
2019-01-29 17:18:40 -0600

    Validación envio de correo solo al guardar invitado

--- common/models/tb-invitados.js
+++ common/models/tb-invitados.js
@@ -6,16 +6,19 @@ var senderAddress = "abraham.saldivar@vun.mx";
 module.exports = function(Tbinvitados) {
 	//Manda credenciales de invitado a correo y guarda contraseña en md5
 	Tbinvitados.observe('before save', function(ctx, next) {
-		Tbinvitados.app.models.Email.send({
-			to: ctx.instance.usuario,
-			from: senderAddress,
-			subject: 'Credenciales de invitado',
-			text: 'Usuario: ' + ctx.instance.usuario + '   ' + 'Contraseña: ' + ctx.instance.contrasenia,
-		}, function(err, mail) {
-			console.log('email sent!');
-			cb(err);
-		});
-		ctx.instance.contrasenia = md5(ctx.instance.contrasenia);
+		//GUARDAR
+		if (ctx.instance) {
+			Tbinvitados.app.models.Email.send({
+				to: ctx.instance.usuario,
+				from: senderAddress,
+				subject: 'Credenciales de invitado',
+				text: 'Usuario: ' + ctx.instance.usuario + '   ' + 'Contraseña: ' + ctx.instance.contrasenia,
+			}, function(err, mail) {
+				console.log('email sent!');
+				cb(err);
+			});
+			ctx.instance.contrasenia = md5(ctx.instance.contrasenia);
+		}
 		next();
 	});
 };
